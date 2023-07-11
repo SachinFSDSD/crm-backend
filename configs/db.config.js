a3e3bdafaffc20efc7ba1166bc56b8ad9383aad4
SachinFSDSD <133794216+SachinFSDSD@users.noreply.github.com>
2023-07-11 20:09:57 +0530

    Update db.config.js

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   DB_NAME: "crm_db",
-  DB_URL: process.env.MONGODB_URI || "mongodb://localhost/crm_db",
+  DB_URL: "mongodb+srv://Sachin:welcome1@crm-backend.snwrxbq.mongodb.net/",
 };
