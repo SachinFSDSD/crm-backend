32de440ec2c3d6c13d79e85ba08d1317a797649e
naval-shekhawat <vijay.jshekhawat@gmail.com>
2023-07-12 03:37:47 +0530

    added

--- utils/NotificationClient.js
+++ utils/NotificationClient.js
@@ -1,22 +1,26 @@
-var Client = require('node-rest-client').Client;
+var Client = require("node-rest-client").Client;
 
 var client = new Client();
 
 module.exports = (ticketId, subject, content, emailIds, requester) => {
-    var reqBody = {
-        ticketId: ticketId,
-        subject: subject,
-        content: content,
-        recepientEmails: emailIds,
-        requester: requester
-    }
+  var reqBody = {
+    ticketId: ticketId,
+    subject: subject,
+    content: content,
+    recepientEmails: emailIds,
+    requester: requester,
+  };
 
-    var args = {
-        data: reqBody,
-        headers: {"Content-Type": "application/json"}
-    }
+  var args = {
+    data: reqBody,
+    headers: { "Content-Type": "application/json" },
+  };
 
-    client.post("https://notification-service-zyxe.onrender.com/notificationService/api/v1/notification", args, function (data, response){
-        console.log(data)
-    })
-}
+  client.post(
+    "https://notification-service-9bqw.onrender.com/notificationService/api/v1/notification",
+    args,
+    function (data, response) {
+      console.log(data);
+    }
+  );
+};
