428e43026ea09f604f5e0c8ed858166536c78cf3
SachinFSDSD <rajusachin090@gmail.com>
2024-02-16 21:47:29 +0530

    first

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   DB_NAME: "crm_db",
-  DB_URL: "mongodb://localhost/crm_db",
+  DB_URL:
+    "mongodb+srv://rajusachin090:<password>@cluster0.vahqcu0.mongodb.net/",
+  // "mongodb://localhost/crm_db",
 };
